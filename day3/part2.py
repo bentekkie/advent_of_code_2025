980a5edda991c14d59b6c7469f2ebdd215765ab8
Ben Segall <bentekkie@google.com>
2025-12-03 09:26:34 -0500

    day3 part 2

--- day3/part2.py
+++ day3/part2.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 
-print('part2')
+inp = "input.txt"
+
+with open(inp,"r") as f:
+	banks = [[int(x) for x in line.strip()] for line in f]
+
+s = 0
+for bank in banks:
+  prev = [0 for _ in range(13)]
+  for x in bank:
+    prev = [max(prev[idx], prev[idx-1]*10+x) if idx > 0 else prev[idx] for idx in range(13)]
+  s += prev[-1]
+print('part 2', s)
