3b5da816aac1c3aa9d7fef8c2e4ee9c35d7782c4
Benjamin Segall <bentekkie@gmail.com>
2025-12-08 08:02:16 -0500

    day 8

---
+++ day8/part2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from itertools import combinations
+from math import sqrt
+
+inp = "input.txt"
+
+
+def dist(p1, p2):
+    return sqrt(sum((a - b) ** 2 for a, b in zip(p1, p2)))
+
+
+with open(inp, "r") as f:
+    coords = [tuple(int(x) for x in line.strip().split(",")) for line in f]
+
+
+pairs = sorted((dist(a, b), a, b) for a, b in combinations(coords, 2))
+
+cir = {i: {coord} for i, coord in enumerate(coords)}
+cc = {coord: i for i, coord in enumerate(coords)}
+for _, a, b in pairs:
+    if cc[a] == cc[b]:
+        continue
+    n = min(cc[a], cc[b])
+    other = max(cc[a], cc[b])
+    cir[n] |= cir[other]
+    del cir[other]
+    for c in cir[n]:
+        cc[c] = n
+    if len(cir) == 1:
+        break
+print("part2", a[0] * b[0])
