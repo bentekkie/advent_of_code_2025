1f6d226918eb81056b1f1c82f034cf02be05cef0
Benjamin Segall <bentekkie@gmail.com>
2025-12-09 11:11:24 +0000

    day9 part2

--- day9/part2.py
+++ day9/part2.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 from itertools import combinations
-
+from shapely import LinearRing, box, Polygon
 
 inp = "input.txt"
 
 with open(inp, "r") as f:
     pnts = [tuple(int(x) for x in line.strip().split(",")) for line in f]
+ring = Polygon(LinearRing(pnts))
+
 l = 0
 for (ax, ay), (bx, by) in combinations(pnts, 2):
-    l = max((1 + abs(ax - bx)) * (1 + abs(ay - by)), l)
+    xmin, xmax = min(ax, bx), max(ax, bx)
+    ymin, ymax = min(ay, by), max(ay, by)
+    b = box(xmin, ymin, xmax, ymax)
+    if ring.contains(b):
+        l = max((1 + abs(ax - bx)) * (1 + abs(ay - by)), l)
 
-print("part1", l)
+print("part2", l)
